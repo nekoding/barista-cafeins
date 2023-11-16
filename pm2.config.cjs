de89ab454882adbb5835a32dee9ae9cf16468c9e
Enggar Tivandi <enggartivandi@outlook.com>
2023-11-16 18:08:20 +0700

    chore: add pm2 config

---
+++ pm2.config.cjs
@@ -0,0 +1,28 @@
+module.exports = {
+  apps: [
+    {
+      name: 'sync projects',
+      script: './app.ts',
+      args: ['barista:sync', 'project'],
+      interpreter: 'bun',
+      cron_restart: '*/30 * * * *',
+      max_memory_restart: '100M',
+    },
+    {
+      name: 'sync sitepoints',
+      script: './app.ts',
+      args: ['barista:sync', 'sitepoint'],
+      interpreter: 'bun',
+      cron_restart: '*/30 * * * *',
+      max_memory_restart: '100M',
+    },
+    {
+      name: 'sync assets',
+      script: './app.ts',
+      args: ['barista:sync', 'asset'],
+      interpreter: 'bun',
+      cron_restart: '*/30 * * * *',
+      max_memory_restart: '100M',
+    },
+  ],
+}
