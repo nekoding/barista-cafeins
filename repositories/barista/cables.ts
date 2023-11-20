50fbb37539e4bfa24d29d066fe028ae8a422ed18
Enggar Tivandi <enggartivandi@outlook.com>
2023-11-20 17:28:06 +0700

    chore: limit take data 100

--- repositories/barista/cables.ts
+++ repositories/barista/cables.ts
@@ -1,13 +1,18 @@
 import type { Cable } from '../../prisma/barista/barista-client'
 import { baristaClient } from '../../utils/database'
 
-const getCablesUnmigrated = async (): Promise<Cable[]> => {
+const getCablesUnmigrated = async (
+  limit: number = 100,
+  offset: number = 0,
+): Promise<Cable[]> => {
   return await baristaClient.cable.findMany({
     where: {
       is_migrated: false,
       cafeins_uuid: null,
       status: null,
     },
+    take: limit,
+    skip: offset,
   })
 }
 
--- repositories/barista/segments.ts
+++ repositories/barista/segments.ts
@@ -1,13 +1,18 @@
 import type { Segment } from '../../prisma/barista/barista-client'
 import { baristaClient } from '../../utils/database'
 
-const getSegmentsUnmigrated = async (): Promise<Segment[]> => {
+const getSegmentsUnmigrated = async (
+  limit: number = 100,
+  offset: number = 0,
+): Promise<Segment[]> => {
   return await baristaClient.segment.findMany({
     where: {
       is_migrated: false,
       cafeins_uuid: null,
       status: null,
     },
+    take: limit,
+    skip: offset,
   })
 }
 
