83169ad0215e296014ad704646d8f84ab4ad321c
Enggar Tivandi <enggartivandi@outlook.com>
2023-11-28 11:00:02 +0700

    chore: add debugging script

---
+++ debug.ts
@@ -0,0 +1,5 @@
+console.log('BARISTA DB URL', process.env.DATABASE_BARISTA_URL)
+console.log('CAFEINS DB URL', process.env.DATABASE_CAFEINS_URL)
+console.log('SITEPOINT CATEGORY ID', process.env.SITEPOINT_CATEGORY_ID)
+console.log('PROJECT GROUP ID', process.env.PROJECT_PROJECT_GROUP_ID)
+console.log('SITE CATEGORY ID', process.env.SITE_CATEGORY_ID)
