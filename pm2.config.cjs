9bd6c31448870826230cee01826818b6f7b9376c
Enggar Tivandi <enggartivandi@outlook.com>
2023-11-28 13:43:11 +0700

    chore: add delay 30 minutes before restart

--- pm2.config.cjs
+++ pm2.config.cjs
@@ -5,54 +5,66 @@ module.exports = {
       script: './app.ts',
       args: ['barista:sync', 'project'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
     {
       name: 'sync sitepoints',
       script: './app.ts',
       args: ['barista:sync', 'sitepoint'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
     {
       name: 'sync assets',
       script: './app.ts',
       args: ['barista:sync', 'asset'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
     {
       name: 'sync routes',
       script: './app.ts',
       args: ['barista:sync', 'route'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
     {
       name: 'sync cables',
       script: './app.ts',
       args: ['barista:sync', 'cable'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
     {
       name: 'sync segments',
       script: './app.ts',
       args: ['barista:sync', 'segment'],
       interpreter: 'bun',
-      cron_restart: '*/30 * * * *',
+      cron_restart: '*/30 * * * *', // if fail restart app in 30 minutes
       instances: 1,
-      autorestart: false,
+      autorestart: true,
+      exec_mode: 'fork',
+      restart_delay: 1_800_000, // delay 30 minute before restart process after done
     },
   ],
 }
